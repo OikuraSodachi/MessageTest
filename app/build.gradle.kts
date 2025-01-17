dabd99adafe60470cb75d493ea32f50a21efae32
Todokanai <bh951201@gmail.com>
2025-01-17 16:02:04 +0900

    firebase

---
+++ app/build.gradle.kts
@@ -0,0 +1,98 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
+    id("com.google.gms.google-services")
+}
+
+android {
+    namespace = "com.todokanai.messagetest"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.todokanai.messagetest"
+        minSdk = 30
+        targetSdk = 31
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    buildFeatures {
+        compose = true
+        viewBinding = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.4"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+dependencies {
+    implementation("androidx.recyclerview:recyclerview-selection:1.1.0")
+    val composeBom = platform("androidx.compose:compose-bom:2024.06.00")
+    val roomVersion = "2.6.1"
+    val glideVersion = "4.13.0"
+
+    implementation (composeBom)
+    testImplementation (composeBom)
+
+    implementation ("androidx.core:core-ktx:1.9.0")
+    implementation ("androidx.lifecycle:lifecycle-common:2.8.4")
+    implementation ("androidx.activity:activity-compose:1.9.1")
+
+    testImplementation ("junit:junit:4.13.2")
+    androidTestImplementation ("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.1")
+    implementation ("androidx.lifecycle:lifecycle-runtime-compose-android:2.8.4")
+    implementation ("androidx.constraintlayout:constraintlayout:2.1.4")     // enable ConstraintLayout
+    implementation ("com.google.android.material:material:1.12.0")
+    implementation ("androidx.fragment:fragment-ktx:1.7.1")
+
+    implementation ("androidx.room:room-runtime:$roomVersion")
+    kapt ("androidx.room:room-compiler:$roomVersion")
+    implementation ("androidx.room:room-ktx:$roomVersion")
+
+    implementation ("androidx.legacy:legacy-support-v4:1.0.0")
+
+    debugImplementation ("androidx.compose.ui:ui-tooling")
+    implementation ("androidx.compose.ui:ui:1.6.8")
+    implementation ("androidx.compose.material3:material3:1.2.1")
+
+    implementation ("com.squareup.picasso:picasso:2.8")
+    implementation ("androidx.hilt:hilt-navigation-compose:1.2.0")
+
+    implementation ("io.coil-kt:coil-compose:2.1.0")
+
+    implementation ("androidx.constraintlayout:constraintlayout-compose:1.0.1")     // ConstraintLayout for compose
+    implementation ("androidx.appcompat:appcompat:1.6.1")
+
+    implementation ("com.github.bumptech.glide:glide:$glideVersion")
+    annotationProcessor ("com.github.bumptech.glide:compiler:$glideVersion")
+    implementation ("com.google.dagger:hilt-android:2.44")
+    kapt ("com.google.dagger:hilt-android-compiler:2.44")
+    implementation ("androidx.datastore:datastore-preferences:1.1.0-alpha01")       // enable DataStore
+
+    //implementation ("com.google.firebase:firebase-admin:9.2.0")
+    implementation(platform("com.google.firebase:firebase-bom:33.7.0"))
+    implementation("com.google.firebase:firebase-analytics")
+}
---
+++ app/src/main/java/com/todokanai/messagetest/components/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.todokanai.messagetest.components
+
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.asLiveData
+import com.todokanai.messagetest.databinding.ActivityMainBinding
+import com.todokanai.messagetest.viewmodel.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy{ActivityMainBinding.inflate(layoutInflater)}
+    private val viewModel : MainViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        viewModel.run{
+            receivedText.asLiveData().observe(this@MainActivity){
+                binding.recieved.text = it
+            }
+        }
+        binding.sendBtn.setOnClickListener {
+            viewModel.send(binding.inputText.text.toString())
+        }
+
+
+        setContentView(binding.root)
+    }
+}
