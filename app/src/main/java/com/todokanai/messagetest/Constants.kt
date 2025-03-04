e0f519fe51a1e410b4eacdb5622aded9ee83ea6f
Todokanai <bh951201@gmail.com>
2025-03-04 09:46:08 +0900

    code cleanup
    Safe

--- app/src/main/java/com/todokanai/messagetest/Constants.kt
+++ app/src/main/java/com/todokanai/messagetest/Constants.kt
@@ -3,9 +3,9 @@ package com.todokanai.messagetest
 import android.app.NotificationManager
 
 object Constants {
-    const val CHANNEL_ID = "Todokanai_MessageTest_ID"
-    /** use this for channelId parameter of [android.app.Notification.Builder] **/
-    const val NOTIFICATION_CHANNEL_NAME : String = "Todokanai_MessageTest_Channel"
+    const val CHANNEL_ID : String = "Todokanai_MessageTest_ID"
+
+    val NOTIFICATION_CHANNEL_NAME : CharSequence = "Todokanai_MessageTest_Channel"
 
     const val CHANNEL_IMPORTANCE : Int = NotificationManager.IMPORTANCE_DEFAULT
 
--- app/src/main/java/com/todokanai/messagetest/Objects.kt
+++ app/src/main/java/com/todokanai/messagetest/Objects.kt
@@ -3,6 +3,6 @@ package com.todokanai.messagetest
 import android.app.NotificationChannel
 
 object Objects {
-    val testChannel = NotificationChannel( Constants.NOTIFICATION_CHANNEL_NAME, Constants.NOTIFICATION_CHANNEL_NAME, Constants.CHANNEL_IMPORTANCE)
+    val testChannel = NotificationChannel(Constants.CHANNEL_ID,Constants.NOTIFICATION_CHANNEL_NAME, Constants.CHANNEL_IMPORTANCE)
 
 }
