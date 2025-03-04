288bbb2e8fb94a35830a8e3029a5e7322e41e39f
Todokanai <bh951201@gmail.com>
2025-03-04 09:33:26 +0900

    code cleanup
    Safe

--- app/src/main/java/com/todokanai/messagetest/Objects.kt
+++ app/src/main/java/com/todokanai/messagetest/Objects.kt
@@ -1,4 +1,8 @@
 package com.todokanai.messagetest
 
+import android.app.NotificationChannel
+
 object Objects {
+    val testChannel = NotificationChannel( Constants.NOTIFICATION_CHANNEL_NAME, Constants.NOTIFICATION_CHANNEL_NAME, Constants.CHANNEL_IMPORTANCE)
+
 }
--- app/src/main/java/com/todokanai/messagetest/di/Etc.kt
+++ app/src/main/java/com/todokanai/messagetest/di/Etc.kt
@@ -1,8 +1,7 @@
 package com.todokanai.messagetest.di
 
-import android.app.NotificationChannel
 import android.app.NotificationManager
-import com.todokanai.messagetest.Constants
+import com.todokanai.messagetest.Objects
 import com.todokanai.messagetest.notifications.NotificationsNew
 import dagger.Module
 import dagger.Provides
@@ -17,9 +16,7 @@ class Etc {
     @Provides
     @Singleton
     fun provideNotificationsNew(notificationManager: NotificationManager):NotificationsNew{
-        val channel = NotificationChannel( Constants.NOTIFICATION_CHANNEL_NAME, Constants.NOTIFICATION_CHANNEL_NAME, Constants.CHANNEL_IMPORTANCE)
-
-        return NotificationsNew(notificationManager,channel).apply {
+        return NotificationsNew(notificationManager, Objects.testChannel).apply {
             createNotificationChannel()
         }
     }
